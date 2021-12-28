0f4a00e50cb8e8400071ea8df9e7e76d57be45d7
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-28 22:49:20 +0300

    fixed excpet

--- bot/commands/mailing.py
+++ bot/commands/mailing.py
@@ -29,10 +29,9 @@ async def set_mailing_text(message: Message, state: FSMContext):
     for user in users:
         try:
             await bot.send_message(chat_id=user.username, text=message.text)
-        except (UserDeactivated, BotBlocked,):
+        except (BotBlocked, UserDeactivated, ChatNotFound):
             blocked_user = await UserData.objects.get(username=user.username)
             await blocked_user.update(is_blocked=True)
-        except ChatNotFound:pass
     globals.is_mailing = False
     end_time = int((dt.now() - start_time).total_seconds())
     return await message.answer(text="Рассылка завершена!\n"f"⏱Минут: {end_time // 60} Секунд: {end_time % 60}")
