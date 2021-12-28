1379e3bd3d17c0a6c572df871919d24a9630220a
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-28 22:33:56 +0300

    add Mailing states

--- bot/states/states.py
+++ bot/states/states.py
@@ -1,5 +1,7 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class Attack(StatesGroup):
-    set_symbol = State()
     set_phone_call = State()
+
+class Mailing(StatesGroup):
+    set_mailing_text_call = State()
